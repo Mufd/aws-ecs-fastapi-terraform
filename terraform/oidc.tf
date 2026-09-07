92408dcc90ad08fdb78d5df06cbef78fa8de8598
Mufd <108560178+Mufd@users.noreply.github.com>
2026-09-07 01:22:53 -0400

    feat: add OIDC provider and IAM role for GitHub Actions workflow for AWS ECS deployment

---
+++ terraform/oidc.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1c58a3a8518e8759bf075b76b750d4f2df264fcd", "6938fd4d98bab03faadb97b34396831e3780aea1"] # Официальные отпечатки GitHub
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:mufd/aws-ecs-fastapi-terraform:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
